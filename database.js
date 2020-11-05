4eef4a918a56444763b2d5990e44eb02a0172ef1
ericmatz <ericmatz1@gmail.com>
2020-11-05 12:07:17 -0600

    Update database.js
    Refactored index variable to indexName, getRecordsObObjectStore skeleton, converted addRecord to addRecords

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 /**
- * Opens a connection to an IndexedDB database
- * @param {string} database_name Name of IndexedDB to be opened
+ * Opens a connection to an indexedDB database
+ * @param {string} database_name Name of indexedDB to be opened
  * @param {number} database_version Version of database to be opened
  * @param {function} upgrade_function Function to be executed if database needs to be upgraded
  * @returns {Promise} Resolve => IDBDatabase | Reject => Error
@@ -30,13 +30,13 @@ function openDB(database_name, database_version, upgrade_function) {
 }
 
 /**
- * Adds a single object to the specificed objectStore
+ * Adds record objects to the specificed objectStore
  * @param {IDBDatabase} database Initiliazed database
  * @param {string} storeName Name of objectStore where transactions will be occurring
- * @param {object} data Object to be inserted into the objectStore
+ * @param {[object]} data Object to be inserted into the objectStore
  * @returns {Promise} Resolve => String | Reject => Error
  */
-function addRecord(database, storeName, data) {
+function addRecords(database, storeName, data) {
     return new Promise(function (resolve, reject) {
         let transaction = database.transaction(storeName, "readwrite");
 
@@ -55,27 +55,30 @@ function addRecord(database, storeName, data) {
 
         let objectStore = transaction.objectStore(storeName);
 
-        let request = objectStore.add(data);
+        data.forEach(record => {
+            let request = objectStore.add(record);
 
-        request.onsuccess = function () {
-            console.log("Add Request Succesful");
-        };
+            request.onsuccess = function () {
+                console.log("Add Request Succesful");
+            };
 
-        request.onerror = function (event) {
-            console.log(`Error: addRecord - Request Failed: addRecord - Supplied Data: {${database_name},${database_version},${upgrade_function}} Code: ${event.target.errorCode} Error: ${event.target.error}`)
-            reject(`Error: addRecord - Request Failed: Supplied Data: {${database_name},${database_version},${upgrade_function}} Code: ${event.target.errorCode} Error: ${request.error}`)
-        };
+            request.onerror = function (event) {
+                reject(`Error: addRecord - Request Failed: Supplied Data: {${database_name},${database_version},${upgrade_function}} Code: ${event.target.errorCode} Error: ${request.error} Record: ${record}`)
+            };
+
+        })
+
     });
 }
 
 /**
- * Deletes records on a given index and key from the specified objectStore
+ * Deletes records on a given Index and key from the specified objectStore
  * @param {IDBDatabase} database Initialized database
  * @param {string} storeName Name of ObjectStore where transactions will be occurring
- * @param {IDBIndex} index An Index from a provided ObjectStore
+ * @param {string} indexName An Index from a provided ObjectStore
  * @param {any} key Key being looked for
  */
-function deleteRecords(database, storeName, index, key) {
+function deleteRecords(database, storeName, indexName, key) {
     return new Promise(function (resolve, reject) {
         let transaction = database.transaction("records", "readwrite");
 
@@ -85,29 +88,25 @@ function deleteRecords(database, storeName, index, key) {
         };
 
         transaction.onabort = function (event) {
-            reject(`Error: deleteRecords - Transaction Aborted - Supplied Data: {${database},${storeName},${index},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
+            reject(`Error: deleteRecords - Transaction Aborted - Supplied Data: {${database},${storeName},${indexName},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
         }
 
         transaction.onerror = function (event) {
-            reject(`Error: deleteRecords - Transaction Failed - Supplied Data: {${database},${storeName},${index},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
+            reject(`Error: deleteRecords - Transaction Failed - Supplied Data: {${database},${storeName},${indexName},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
         };
 
-        let objectStore = transaction.objectStore(storeName);
-
-        let objectStoreIndex = objectStore.index(index);
-
-        let objectStoreCursor = objectStoreIndex.openCursor();
+        let objectStoreCursor = transaction.objectStore(storeName).index(indexName).openCursor();
 
         objectStoreCursor.onsuccess = function (event) {
             let cursor = event.target.result;
             if (cursor) {
-                if (cursor.value[index] === key) {
+                if (cursor.value[indexName] === key) {
                     let request = cursor.delete();
                     request.onsuccess = function () {
                         console.log('Record Deleted:', cursor.value);
                     }
                     request.onerror = function (event) {
-                        reject(`Error: deleteRecords - Request Failed - Cursor: ${cursor} Supplied Data: {${database},${storeName},${index},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
+                        reject(`Error: deleteRecords - Request Failed - Cursor: ${cursor} Supplied Data: {${database},${storeName},${indexName},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
                     }
                 }
                 cursor.continue();
@@ -116,7 +115,7 @@ function deleteRecords(database, storeName, index, key) {
         }
 
         objectStoreCursor.onerror = function (event) {
-            resolve(`Error: deleteRecords - Cursor Failed - Supplied Data: {${database},${storeName},${index},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
+            resolve(`Error: deleteRecords - Cursor Failed - Supplied Data: {${database},${storeName},${indexName},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
         }
     });
 }
@@ -125,11 +124,11 @@ function deleteRecords(database, storeName, index, key) {
  * Returns all records from the provided ObjectStore searching on the provided Index
  * @param {IDBDatabase} database Initialized database
  * @param {string} storeName Name of ObjectStore where transactions will be occurring
- * @param {string} index An Index from a provided ObjectStore
+ * @param {string} indexName An Index from a provided ObjectStore
  * @param {IDBKeyRange | any} key Key being looked for
  * @returns {Promise} Resolve => Array of Objects | Reject => Reason
  */
-function getRecordsOnIndex(database, storeName, index, key) {
+function getRecordsOnIndex(database, storeName, indexName, key) {
     return new Promise(function (resolve, reject) {
         let transaction = database.transaction(storeName, "readonly");
 
@@ -138,18 +137,14 @@ function getRecordsOnIndex(database, storeName, index, key) {
         };
 
         transaction.onabort = function (event) {
-            reject(`Error: getRecordsOnIndex - Transaction Aborted - Supplied Data: {${database},${storeName},${index},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
+            reject(`Error: getRecordsOnIndex - Transaction Aborted - Supplied Data: {${database},${storeName},${indexName},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
         }
 
         transaction.onerror = function (event) {
-            reject(`Error: getRecordsOnIndex - Transaction Failed - Supplied Data: {${database},${storeName},${index},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
+            reject(`Error: getRecordsOnIndex - Transaction Failed - Supplied Data: {${database},${storeName},${indexName},${key} Code: ${event.target.errorCode} Error: ${event.target.error}`)
         };
 
-        let objectStore = transaction.objectStore(storeName);
-
-        let objectStoreIndex = objectStore.index(index);
-
-        let request = objectStoreIndex.getAll(key);
+        let request = transaction.objectStore(storeName).index(indexName).getAll(key);
 
         request.onsuccess = function (event) {
             console.log("Get All Request Succesful");
@@ -157,10 +152,20 @@ function getRecordsOnIndex(database, storeName, index, key) {
         };
 
         request.onerror = function (event) {
-            reject(`Error: getRecordsOnIndex - Request Failed - Supplied Data: {${database},${storeName},${index},${key}} Code: ${event.target.errorCode} Error: ${event.target.error}`)
+            reject(`Error: getRecordsOnIndex - Request Failed - Supplied Data: {${database},${storeName},${indexName},${key}} Code: ${event.target.errorCode} Error: ${event.target.error}`)
         };
 
     });
+}
+
+/**
+ * Returns all records from the provided ObjectStore
+ * @param database 
+ * @param storeName 
+ * @returns {Promise} Resolve => Array of Object | Reject => Reason
+ */
+function getRecordsOnObjectStore(database, storeName) {
+    return new Promise(function (resolve, reject) {})
 }
 
 export {openDB, addRecord, deleteRecords, getRecordsOnIndex}
